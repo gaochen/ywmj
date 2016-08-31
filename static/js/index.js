4966b16163ec652fcc2fef39c889172088f95f06
gaochen <gc_andrew@sina.com>
2016-08-31 18:36:21 +0800

    20160831

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,17 @@ $(function() {
     /* 首页头部导航切换 */
    (function() {
         $(".index-nav a").on("tap", function() {
+            var index = $(this).index();
+
+            if (index === 1) {
+                $(".content").addClass("data-active");
+            }
+            else {
+                $(".content").removeClass("data-active");
+            }
+
             $(this).addClass("active").siblings("a").removeClass("active");
+            $(".index-tab").eq(index).addClass("show").siblings(".index-tab").removeClass("show");
         });
    })();
 });
